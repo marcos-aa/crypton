f214f4649bea6a6233b739a21e8ac2d5c936e021
Marcos <marcosandrade.it@gmail.com>
2024-02-23 21:52:08 -0300

    UI: display custom windows of time when comparing ticks

--- frontend/src/components/views/Dashboard/CompareTicks/TimeWindows/index.tsx
+++ frontend/src/components/views/Dashboard/CompareTicks/TimeWindows/index.tsx
@@ -1,26 +1,45 @@
+import { MouseEvent } from "react";
 import styles from "./styles.module.scss";
 
-export default function TimeWindows() {
+interface TimeProps {
+  addWindow(e: MouseEvent<HTMLLIElement>): void;
+}
+
+export default function TimeWindows({ addWindow }: TimeProps) {
   return (
     <div id={styles.timeWindow}>
       <span> Add a window to table of values </span>
-      <ul id={styles.commonWindows}>
-        <li className={styles.prewindow}> 1M </li>
-        <li className={styles.prewindow}> 30M </li>
-        <li className={styles.prewindow}> 1H </li>
-        <li className={styles.prewindow}> 6H </li>
-        <li className={styles.prewindow}> 12H </li>
-        <li className={styles.prewindow}> 1D </li>
-        <li className={styles.prewindow}> 3D </li>
-      </ul>
+      <div id={styles.commonWindows}>
+        <li onClick={addWindow} className={styles.prewindow}>
+          1m
+        </li>
+        <li onClick={addWindow} className={styles.prewindow}>
+          30m
+        </li>
+        <li onClick={addWindow} className={styles.prewindow}>
+          1h
+        </li>
+        <li onClick={addWindow} className={styles.prewindow}>
+          6h
+        </li>
+        <li onClick={addWindow} className={styles.prewindow}>
+          12h
+        </li>
+        <li onClick={addWindow} className={styles.prewindow}>
+          1d
+        </li>
+        <li onClick={addWindow} className={styles.prewindow}>
+          3d
+        </li>
+      </div>
 
       <div id={styles.customWindow}>
         <label htmlFor="window"> Select a custom window </label>
         <input name="window" type="number" />
         <select name="unit" id={styles.timeUnit}>
-          <option value="M"> Minutes </option>
-          <option value="H"> Hours </option>
-          <option value="D"> Days </option>
+          <option value="m"> Minutes </option>
+          <option value="h"> Hours </option>
+          <option value="d"> Days </option>
         </select>
       </div>
     </div>
--- frontend/src/components/views/Dashboard/CompareTicks/index.tsx
+++ frontend/src/components/views/Dashboard/CompareTicks/index.tsx
@@ -1,7 +1,7 @@
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { QueryClient } from "@tanstack/react-query";
-import { useState } from "react";
+import { MouseEvent, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import api from "../../../../services/api";
 import { Prices, StreamData } from "../../../../utils/datafetching";
@@ -19,24 +19,62 @@ export const ticksLoader = (qc: QueryClient) => async () => {
 
   const res = {
     "7d": data,
-    current: qc.getQueryData<StreamData>(["streams"]).tickers,
+    "1s": qc.getQueryData<StreamData>(["streams"]).tickers,
   };
   return res;
 };
 
 interface WindowTickers {
-  [key: "7d" | "current" | string]: {
+  [key: "7d" | "1s" | string]: {
     [key: string]: Prices;
   };
 }
 export default function CompareTicks() {
   const data = useLoaderData() as WindowTickers;
   const [values, setValues] = useState(data);
-  const [windows, setWindows] = useState(["7d", "current"]);
+  const [windows, setWindows] = useState(["1s", "7d"]);
   const [edit, setEdit] = useState(false);
 
   const editWindows = () => setEdit((prev) => !prev);
 
+  const addWindow = (e: MouseEvent<HTMLLIElement>) => {
+    const vtext = e.currentTarget.innerHTML;
+    const lastInd = vtext.length - 1;
+    const cUnit = vtext[lastInd];
+    const multip = {
+      m: 1,
+      h: 60,
+      d: 1440,
+    };
+    const value = Number(vtext.slice(0, lastInd)) * multip[cUnit];
+
+    let start = 1,
+      end = windows.length - 1;
+
+    setWindows((curr) => {
+      const wins = [...curr];
+      let mid = end;
+
+      while (start < end) {
+        mid = Math.floor((start + end) / 2);
+        const midtx = wins[mid];
+
+        if (midtx == vtext) {
+          end = 0;
+          break;
+        }
+
+        const midValue = midtx.slice(0, midtx.length - 1);
+        const midMult = Number(midValue) * multip[midtx[midtx.length - 1]];
+
+        if (value > midMult) start = mid + 1;
+        else end = mid;
+      }
+
+      if (end != 0) wins.splice(start, 0, vtext);
+      return wins;
+    });
+  };
   return (
     <ModalContainer predecessor="/dashboard">
       <div id={styles.compareTicks}>
@@ -53,10 +91,10 @@ export default function CompareTicks() {
           <button id={styles.addFrame} type="button" onClick={editWindows}>
             <FontAwesomeIcon icon={faPlus} />
           </button>
-          {edit && <TimeWindows />}
+          {edit && <TimeWindows addWindow={addWindow} />}
         </div>
 
-        {Object.keys(data["current"]).map((symbol) => {
+        {Object.keys(data["1s"]).map((symbol) => {
           return (
             <div key={symbol} className={styles.symRow}>
               <h2 className={styles.rowTitle}> {symbol} </h2>
