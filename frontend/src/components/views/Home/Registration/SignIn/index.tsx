f4202417b1c6434ec00c3142d5ab914e751e05a4
Marcos <marcosandrade.it@gmail.com>
2024-04-11 13:12:06 -0300

    Bugfix: user data not being stored in query after sign in

--- frontend/src/components/views/Home/Registration/SignIn/index.tsx
+++ frontend/src/components/views/Home/Registration/SignIn/index.tsx
@@ -2,12 +2,14 @@ import { useNavigate } from "react-router"
 import { Link } from "react-router-dom"
 import AuthForm from "../../../../AuthForm"
 
-import { UserData } from "shared/usertypes"
+import { useQueryClient } from "@tanstack/react-query"
+import { UIUser, UserData } from "shared/usertypes"
 import api from "../../../../../services/api"
 import { saveHeader } from "../../../../../utils/datafetching"
 import { InputData } from "../../../../../utils/helpers"
 
 export default function SignIn() {
+  const qc = useQueryClient()
   const navigate = useNavigate()
 
   const signIn = async (input: InputData) => {
@@ -26,6 +28,7 @@ export default function SignIn() {
     }
 
     saveHeader(data.accessToken)
+    qc.setQueryData<UIUser>(["user"], () => data.user)
     navigate("/dashboard")
   }
 
