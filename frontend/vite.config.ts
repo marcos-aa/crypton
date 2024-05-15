42630d91e81699c1c3a78b0009486fb89336bccc
Marcos <marcosandrade.it@gmail.com>
2024-05-15 15:20:08 -0300

    Updated vite and react plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,11 @@
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
-
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/vitest-setup.ts",
-    css: {
-      modules: {
-        classNameStrategy: "non-scoped",
-      },
-    },
-    coverage: {
-      provider: "c8",
+  css: {
+    modules: {
+      scopeBehaviour: "local",
     },
   },
   server: {
