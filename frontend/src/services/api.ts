f591035c33ea4ec6415c6cb49cdcd4bb9d20827e
Marcos <marcosandrade.it@gmail.com>
2025-02-22 18:03:45 -0300

    Use custom domain

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -3,7 +3,7 @@ import { saveHeader } from "../utils/datafetching"
 
 const baseURL =
   import.meta.env?.MODE === "production"
-    ? "https://crypton-90x5.onrender.com"
+    ? "https://server.crypton.icu"
     : "http://localhost:3000"
 const api = axios.create({
   baseURL,
